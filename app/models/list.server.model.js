64b3283ff8efecce7bef13395f198039e45342c2
Carlo Miras <mirascarlo934@gmail.com>
2021-04-07 23:25:21 +0800

    Add list schema

---
+++ app/models/list.server.model.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+    Schema = mongoose.Schema;
+
+var ListSchema = new Schema({
+    name: String,
+    description: String,
+    order: {
+        type: Number,
+        min: 0
+    },
+    sectionId: String,
+    categoryId: String
+});
+
+mongoose.model("List", ListSchema);
