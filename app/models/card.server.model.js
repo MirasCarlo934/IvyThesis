2175d862dbe7547fa73dc39ea08761c4d9dd79dc
Carlo Miras <mirascarlo934@gmail.com>
2021-04-07 23:26:29 +0800

    Add card schema

---
+++ app/models/card.server.model.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+Schema = mongoose.Schema;
+
+var CardSchema = new Schema({
+    name: String,
+    description: String,
+    order: {
+        type: Number,
+        min: 0
+    },
+    listId: String
+});
+
+mongoose.model("Card", CardSchema);
