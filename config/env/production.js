b2b1745288bf366580510a1ce3695deb2851e3cf
Carlo Miras <mirascarlo934@gmail.com>
2021-04-10 19:28:49 +0800

    feat: add production.js

---
+++ config/env/production.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // Production configuration options
+    mongodb: {
+        uri: "mongodb+srv://byronicad:mongo1131@symphonycluster.gy7bf.mongodb.net/ivythesis?retryWrites=true&w=majority"
+    }
+    api: {
+        host: "http://34.121.110.97:3000",
+        categoriesPath: "/categories",
+        sectionsPath: "/sections",
+        listsPath: "/lists",
+        cardsPath: "/cards"
+    }
+}
