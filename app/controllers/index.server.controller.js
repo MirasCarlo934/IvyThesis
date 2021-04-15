772619f0a8a20a4057bea918cec889cfa874d585
Carlo Miras <mirascarlo934@gmail.com>
2021-04-15 13:27:39 +0800

    feat: add modal for card editing

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,7 +1,11 @@
 var config = require("../../config/config")
 
 exports.render = function(req, res) {
+    if (!req.query.env) {
+        req.query.env = "production";
+    }
     res.render("index", {
+        env: req.query.env,
         title: "Hello World",
         api: config.api
     });
