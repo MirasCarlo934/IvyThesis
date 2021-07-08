ef02f09f6f28e76d84cb9308530d6c28cd8ffa53
Carlo Miras <mirascarlo934@gmail.com>
2021-07-08 09:37:25 +0800

    fix: update production API URL

--- config/env/production.js
+++ config/env/production.js
@@ -4,7 +4,7 @@ module.exports = {
         uri: "mongodb+srv://byronicad:mongo1131@symphonycluster.gy7bf.mongodb.net/ivythesis?retryWrites=true&w=majority"
     },
     api: {
-        host: "http://34.121.110.97:3000",
+        host: "http://35.198.231.19:3000",
         categoriesPath: "/categories",
         sectionsPath: "/sections",
         listsPath: "/lists",
